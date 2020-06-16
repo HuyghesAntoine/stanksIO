cad81ae0f521b4902fa45fccaa519d265e225a65
theodouble0 <theodouble0@gmail.com>
2020-06-16 09:58:12 +0200

    changement de Bullet

--- sources/engine/Bullet.js
+++ sources/engine/Bullet.js
@@ -1,7 +1,14 @@
-class Bullet {
-    constructor(damage, direction){
-        this.damage = damage;
+const Entity = require("./Entity");
+
+class Bullet extends Entity{
+    constructor(color, size, mapSize, x, y, attack, direction){
+        super(color, size, mapSize, x, y, 1, speed, attack);
         this.direction = direction;
+    }
+
+    move(){
+        this.x += (Math.cos(this.direction) * this.speed);
+        this.y += (Math.sin(this.direction) * this.speed);
     }
 }
 
