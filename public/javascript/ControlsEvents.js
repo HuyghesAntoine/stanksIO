d56d62f0cb9507eef6da81d0729fea65bd556f0a
vincent villette <v.villette976@gmail.com>
2020-06-17 11:51:48 +0200

    reparation du bon monsieur

--- public/javascript/ControlsEvents.js
+++ public/javascript/ControlsEvents.js
@@ -1,18 +1,6 @@
 class ControlsEvents {
     constructor(socket) {
         this.socket = socket;
-        /*this.buttonUp = document.querySelector('#up');
-        this.buttonUp.onclick = (event) => this.onClickMoveUp(event);
-        this.buttonUp.onkeypress = (event) => this.myFunction(event);
-        this.buttonDown = document.querySelector('#down');
-        this.buttonDown.onclick = (event) => this.onClickMoveDown(event);
-        this.buttonRight = document.querySelector('#right');
-        this.buttonRight.onclick = (event) => this.onClickMoveRight(event);
-        this.buttonLeft = document.querySelector('#left');
-        this.buttonLeft.onclick = (event) => this.onClickMoveLeft(event);
-        this.buttonShoot = document.querySelector('#fire');
-        this.buttonShoot.onclick = (event) => this.onClickShoot(event);
-        */
         document.getElementById('controlPage').addEventListener('keydown', (event) => this.onKeyDown(event), false);
     }
     onKeyDown(event) {
