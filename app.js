c8bcb0caac0b6a4ae59e310c7b8c0cc7ca6b2994
girafou72 <v.villette976@gmail.com>
2020-06-19 23:33:41 +0200

    compte

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const session = require('express-session');
 const router = require('./sources/routes/router');
 
 const app = express();
@@ -11,7 +11,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: '$3CR37',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(function(req,res,next){
+    res.locals.session = req.session;
+    next();
+});
 
 app.use('/', router);
 
---
+++ controller/userController.js
@@ -0,0 +1,23 @@
+const data = require('./userManager.js');
+const path = require('path');
+
+
+
+exports.register = function (req,res){
+    const insc = data.insert_user(req.body.lastname, req.body.firstname, req.body.Jour, req.body.Mois, req.body.Année, req.body.mail, req.body.mot2pass);
+    res.sendFile(path.join(__dirname, "../public/html/index.html"));
+}
+
+exports.sign_in = function(req,res){
+    const conex = data.get_user(req.body.mail, req.body.mot2pass);
+    if(conex){
+        
+        req.session.firstname=conex.firstname;
+        res.redirect('/index?status=success');
+        console.log(conex.firstname);
+    }
+    else{
+        res.redirect('/index?status=error');
+        console.log('erreur');
+    }
+};
