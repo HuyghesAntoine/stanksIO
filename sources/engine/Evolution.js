db48a74464459a793c69a7d5281586aaf0eb8345
Pierre Skibinski <p.skibinski@hotmail.com>
2020-06-22 15:29:47 +0200

    modified evolution values

--- sources/engine/Evolution.js
+++ sources/engine/Evolution.js
@@ -1,7 +1,7 @@
 class Evolution {
     constructor(evolutions){
         this.evolutions = evolutions;
-        this.levelNeeded = 2;
+        this.levelNeeded = 5;
         this.indice = 0;
     }
     add(direction){
@@ -16,7 +16,7 @@ class Evolution {
             else {
                 directions.push(this.evolutions[0]);
                 this.evolutions.splice(0,1);
-                this.levelNeeded += 1;  
+                this.levelNeeded += 5;  
             }
         }
         return directions;
