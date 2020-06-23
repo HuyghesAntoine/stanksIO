1c213b6886f56e75e0fb8b02c354650fe532413a
Pierre Skibinski <p.skibinski@hotmail.com>
2020-06-23 11:52:04 +0200

    resized canvas and modified bonus/factory repop mecanism

--- sources/engine/Factory.js
+++ sources/engine/Factory.js
@@ -17,7 +17,11 @@ class Factory {
     // Function that add a factory entity in an array?
     addEntity() {
         // 10 factory entites maximum in the map.
-        if (this.chrono.isOver(this.delay) && this.entities.length <= 50) {
+        if (this.entities.length > 50 && this.delay <= 20000)
+            this.delay += 100;
+        else if (this.delay > 300)
+            this.delay -= 100;
+        if (this.chrono.isOver(this.delay)) {
             // Add the entity in the array with given properties.
             this.entities.push(new Entity(5, getRandom(0, this.mapSizeX), getRandom(0, this.mapSizeY), 1, '#1FE400', this.mapSizeX, this.mapSizeY));
             // Reset the chrono for a new factory entity to spawn.
