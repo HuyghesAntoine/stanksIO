3929c5ed278cde731cd09ce2a45dc282d2634562
Pierre Skibinski <p.skibinski@hotmail.com>
2020-06-23 14:19:12 +0200

    added helath/xp bar, a star when level up

--- public/javascript/GameCanvas.js
+++ public/javascript/GameCanvas.js
@@ -4,9 +4,27 @@ class GameCanvas {
         this.context = this.canvas.getContext('2d');
     }
 
-    drawLife(tank) {
-        const { x, y, size, color, health, maxHealth } = tank;
+    drawLife(x, y, size, health, maxHealth, level) {
+        let width = 2*size;
+        let height = 7;
+        let border = 3;
+        let ratio = health / maxHealth;
+        let ratioXp = level.xp / level.xpNeeded;
+        let X = x - (width / 2);
+        let Y = y + size + 20;
 
+        this.context.fillStyle = '#000000';
+        this.context.fillRect(X-border, Y-border, width+(2*border), height+(2*border));
+
+        this.context.fillStyle = '#00ff00';
+        this.context.fillRect(X, Y, ratio * width, height * (2 / 3));
+        this.context.fillStyle = '#ff0000';
+        this.context.fillRect(X + (ratio * width), Y, (1 - ratio) * width, height * (2 / 3));
+
+        this.context.fillStyle = '#0000ff';
+        this.context.fillRect(X, Y+(height*2/3), ratioXp * width, height * (1 / 3));
+        this.context.fillStyle = '#cccccc';
+        this.context.fillRect(X + (ratioXp * width), Y+(height*2/3), (1-ratioXp) * width, height * (1 / 3));
     }
 
     drawCannon(tank, canonDirection) {
@@ -14,48 +32,47 @@ class GameCanvas {
         this.context.beginPath();
         this.context.arc((x + Math.cos(look + canonDirection) * (size)), (y + Math.sin(look + canonDirection) * (size)), (size / 2), 0, 2 * Math.PI, false);
         this.context.arc((x + Math.cos(look + canonDirection) * (size * 1.2)), (y + Math.sin(look + canonDirection) * (size * 1.2)), (size / 2), 0, 2 * Math.PI, false);
-        
-        if (typeof(tank.alpha) != "undefined")
-            this.context.fillStyle = 'rgba(0,0,0,'+alpha+')';
+
+        if (typeof (tank.alpha) != "undefined")
+            this.context.fillStyle = 'rgba(0,0,0,' + alpha + ')';
         else
             this.context.fillStyle = 'rgba(0,0,0,1)';
         this.context.fill();
     }
 
-    drawBorders(x,y,size){
+    drawBorders(x, y, size) {
         this.context.beginPath();
-        this.context.arc(x, y, size+2, 0, 2 * Math.PI, false);
+        this.context.arc(x, y, size + 2, 0, 2 * Math.PI, false);
         this.context.fillStyle = '#000000';
         this.context.fill();
     }
 
-   /* drawHealthBar(x,y,size){
-        this.beginPath();
-        this.context.fillRect(x,y, size, size/2);
-        this.context.fillStyle = '#123123';
-        this.context.fill();
-    } */
-
     drawTank(tank) {
-        const { x, y, size, color, alpha } = tank;
+        const { x, y, size, color, alpha, health, maxHealth, level } = tank;
+        this.drawLife(x, y, size, health, maxHealth, level);
         tank.gun.forEach(canon => {
             for (let i = 0; i < canon.ammos.length; i++) {
                 this.drawBullet(canon.ammos[i]);
             }
             this.drawCannon(tank, canon.direction);
         });
-        this.drawBorders(x,y,size);
+        this.drawBorders(x, y, size);
         //this.drawHealthBar(x,y,size);
         this.context.beginPath();
         this.context.arc(x, y, size, 0, 2 * Math.PI, false);
-        console.log(alpha);
-        if (typeof(tank.alpha) != "undefined")
-            this.context.fillStyle = "rgba("+ color.slice(1,3) +',' + color.slice(3,5) +','+ color.slice(5,7) +','+ alpha+")";
+        if (typeof (tank.alpha) != "undefined")
+            this.context.fillStyle = "rgba(" + color.slice(1, 3) + ',' + color.slice(3, 5) + ',' + color.slice(5, 7) + ',' + alpha + ")";
         else
             this.context.fillStyle = color;
         this.context.fill();
+        this.context.font = "12px Arial";
         this.context.textAlign = "center";
-        this.context.fillText(tank.pseudo, x, y - (2 * size - 5));
+        this.context.fillText(tank.pseudo, x, y - (tank.size + 20));
+        if (level.xpPoint>0){
+            this.context.font = "30px Arial";
+            this.context.textAlign = "center";
+            this.context.fillText('*', x, y - (tank.size + 30));
+        }
     }
 
     drawBullet(bullet) {
